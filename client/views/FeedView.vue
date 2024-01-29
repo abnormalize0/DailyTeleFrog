9ad0cf953fb10cb4b78662a30cb620a64d99a006
abnormalize0 <mixailchernov8@gmail.com>
2024-01-29 23:30:06 +0300

    #98 put all vals and method into component

--- client/views/FeedView.vue
+++ client/views/FeedView.vue
@@ -2,116 +2,9 @@
   @import '../css/PreviewStyle.css';
 </style>
 
-
-<script setup>
-  import { ref, onMounted, onUpdated } from 'vue';
-  import { DateTime, Settings } from 'luxon';
-  Settings.defaultLocale = 'ru';
-
-  let PAGE_PER_ARTICLES = 5;
-
-  let page = 0;
-  let post_id = 1;
-  let load_line_id = PAGE_PER_ARTICLES;
-  let allow = 0;
-
-  onMounted(() => {
-    get_posts(page);
-    posts.value.splice(0);
-    load_line_id = PAGE_PER_ARTICLES;
-    page = 0;
-  })
-
-  onUpdated(() => {
-    allow = 1;
-    post_id = 1;
-  })
-
-  document.addEventListener("scroll", () => {
-    if(allow === 0) {
-      return;
-    }
-    let element = document.getElementById('post' + load_line_id);
-    if(element == null) {
-      allow = 0;
-      return;
-    }
-    let load_line_element = element.getBoundingClientRect();
-    if(load_line_element.top <= (window.innerHeight || document.documentElement.clientHeight)) {
-      load_line_id += PAGE_PER_ARTICLES;
-      allow = 0;
-      page++;
-      get_posts(page);
-    }
-  })
-
-  async function like_change(post_id) { //заглушка пока не будет рабочего апи от сервера
-    const request = await fetch("http://127.0.0.1:5000/article/likes", {
-      method: 'POST',
-      headers: {
-        'user-id': localStorage.id,
-        'article-id': post_id,
-      },
-    })
-    let status = await request.json();
-    console.log(status);
-    let like = document.getElementById("like" + post_id);
-    if(((like.innerHTML).split(" ")[1]) == 1) {
-      like.innerHTML = "2";
-    } else {
-      like.innerHTML = "1";
-    }
-  }
-
-  async function dislike_change(post_id, command) { //заглушка пока не будет рабочего апи от сервера
-    const request = await fetch("http://127.0.0.1:5000/article/data", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'user-id': localStorage.id,
-        'article-id': post_id,
-      },
-      body: JSON.stringify({command: {}})
-    })
-    let status = await request.json();
-    console.log(status);
-    let like = document.getElementById("like" + post_id);
-    if(((like.innerHTML).split(" ")[1]) == 1) {
-      like.innerHTML = "2";
-    } else {
-      like.innerHTML = "1";
-    }
-  }
-
-  function time_ago(date) {
-    let seconds = DateTime.now().toUnixInteger() - date / 1000;
-    let current_date = DateTime.now().toObject();
-    let post_date = DateTime.fromMillis(date).toObject();
-    let days = DateTime.now().diff(DateTime.fromMillis(date), ["days"]).toObject();
-    console.log(days)
-    if (current_date.year != post_date.year) {
-      return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric', year: 'numeric'});
-    }
-    if ((days.days < 2)&&(days.days > 1)) {
-      return 'вчера';
-    }
-    if ((current_date.day != post_date.day)||(current_date.month != post_date.month)) {
-      return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric'});
-    }
-    if (seconds <= 10) {
-      return 'только что';
-    }
-    return DateTime.now().minus({ seconds: DateTime.now().toUnixInteger() - date / 1000 }).toRelative();
-  }
-
-  function tooltip_time(date) {
-    return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' });
-  }
-</script>
-
 <template id="post_template">
   <section class="posts-list">
-    <div class="list">
+    <div class="list" @scroll = "handleScroll">
       <div id="feed">
         <div v-for="post in posts" :key="post.id" >
           <router-link :to="{ name: 'post', params: { id: post.article_id } }" custom v-slot="{ navigate }">
@@ -146,7 +39,7 @@
                 <div class="post-misc" >🗪 {{ post.comments_count }}</div>
                 <div class="post-misc" @click="like_change(post.article_id)" :id="'display_like'+post.article_id">↑</div>
                 <div class="post-misc" :id="'like'+post.article_id">{{ post.likes_count }}</div>
-                <div class="post-misc" @click="dislike_change(post.article_id, 'dislike-article')" :id="'display_dislike'+post.article_id">↓</div>
+                <div class="post-misc" @click="dislike_change(post.article_id)" :id="'display_dislike'+post.article_id">↓</div>
               </div>
             </div>
           </router-link>
@@ -157,42 +50,140 @@
 </template>
 
 <script>
-  let posts = ref([]);
-  async function get_posts(page) {
-    const request = await fetch("http://127.0.0.1:5000/pages", {
-      method: 'GET',
-      headers: {
-        'indexes': "~"+page+"~",
-        'user-id': localStorage.id,
-        'include-nonsub': true,
-        'sort-column': 'creation_date',
-        'sort-direction': "descending"
+  import { DateTime, Settings } from 'luxon';
+  Settings.defaultLocale = 'ru';
 
-      }
-    })
-    let json = await request.json();
-    console.log(json);
-    for(let i = 0; i < json.pages[page].length; i++) {
-      // const another_request = await fetch("http://127.0.0.1:5000/article/data", {
-      //   method: 'GET',
-      //   headers: {
-      //     'article-id': json.pages[page][i].id,
-      //     'requested-data': '~dislikes_id~'
-      //   }
-      // });
-      // let new_json = await another_request.json();
-      // console.log(new_json);
-      posts.value.push({
-        id: i,
-        name: decodeURIComponent(json.pages[page][i].name),
-        created: json.pages[page][i].creation_date,
-        preview_content: json.pages[page][i].preview_content,
-        tags: decodeURIComponent(json.pages[page][i].tags).split("~").filter(elm => elm),
-        article_id: json.pages[page][i].id,
-        author_preview: json.pages[page][i].author_preview,
-        likes_count: json.pages[page][i].likes_count,
-        comments_count: json.pages[page][i].comments_count,
-      })
-    }
-  }
+  export default {
+    data() {
+      return {
+        PAGE_PER_ARTICLES:5, 
+        posts:[], 
+        page:0, 
+        post_id:1, 
+        load_line_id:5, 
+        allow:0
+      };
+    },
+    methods: {
+      handleScroll() {
+        if(this.allow === 0) {
+          return;
+        }
+        let element = document.getElementById('post' + this.load_line_id);
+        if(element == null) {
+          this.allow = 0;
+          return;
+        }
+        let load_line_element = element.getBoundingClientRect();
+        if(load_line_element.top <= (window.innerHeight || document.documentElement.clientHeight)) {
+          this.load_line_id += this.PAGE_PER_ARTICLES;
+          this.allow = 0;
+          this.page++;
+          this.get_posts(this.page);
+        }
+      },
+      async get_posts(page) {
+        const request = await fetch("http://127.0.0.1:5000/pages", {
+          method: 'GET',
+          headers: {
+            'indexes': "~"+page+"~",
+            'user-id': localStorage.id,
+            'include-nonsub': true,
+            'sort-column': 'creation_date',
+            'sort-direction': "descending"
+          }
+        })
+        let json = await request.json();
+        console.log(json);
+        for(let i = 0; i < json.pages[page].length; i++) {
+          this.posts.push({
+            id: i,
+            name: decodeURIComponent(json.pages[page][i].name),
+            created: json.pages[page][i].creation_date,
+            preview_content: json.pages[page][i].preview_content,
+            tags: decodeURIComponent(json.pages[page][i].tags).split("~").filter(elm => elm),
+            article_id: json.pages[page][i].id,
+            author_preview: json.pages[page][i].author_preview,
+            likes_count: json.pages[page][i].likes_count,
+            comments_count: json.pages[page][i].comments_count,
+          })
+        }
+      },
+      async like_change(post_id) { //заглушка пока не будет рабочего апи от сервера
+        const request = await fetch("http://127.0.0.1:5000/article/likes", {
+          method: 'POST',
+          headers: {
+            'user-id': localStorage.id,
+            'article-id': post_id,
+          },
+        })
+        let status = await request.json();
+        console.log(status);
+        let like = document.getElementById("like" + post_id);
+        if(((like.innerHTML).split(" ")[1]) == 1) {
+          like.innerHTML = "2";
+        } else {
+          like.innerHTML = "1";
+        }
+      },
+      async dislike_change(post_id) { //заглушка пока не будет рабочего апи от сервера
+        const request = await fetch("http://127.0.0.1:5000/article/data", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'user-id': localStorage.id,
+            'article-id': post_id,
+          },
+        })
+        let status = await request.json();
+        console.log(status);
+        let like = document.getElementById("like" + post_id);
+        if(((like.innerHTML).split(" ")[1]) == 1) {
+          like.innerHTML = "2";
+        } else {
+          like.innerHTML = "1";
+        }
+      },
+      time_ago(date) {
+        let seconds = DateTime.now().toUnixInteger() - date / 1000;
+        let current_date = DateTime.now().toObject();
+        let post_date = DateTime.fromMillis(date).toObject();
+        let days = DateTime.now().diff(DateTime.fromMillis(date), ["days"]).toObject();
+        if (current_date.year != post_date.year) {
+          return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric', year: 'numeric'});
+        }
+        if ((days.days < 2)&&(days.days > 1)) {
+          return 'вчера';
+        }
+        if ((current_date.day != post_date.day)||(current_date.month != post_date.month)) {
+          return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric'});
+        }
+        if (seconds <= 10) {
+          return 'только что';
+        }
+        return DateTime.now().minus({ seconds: DateTime.now().toUnixInteger() - date / 1000 }).toRelative();
+      },
+      tooltip_time(date) {
+        return DateTime.fromMillis(date).toLocaleString({month: 'long', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' });
+      },
+    },
+    mounted() {
+      this.get_posts(this.page);
+      this.posts.splice(0);
+      this.load_line_id = this.PAGE_PER_ARTICLES;
+      this.page = 0;
+
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    updated() {
+      this.allow = 1;
+      this.post_id = 1;
+    },
+    beforeUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+    },
+  };
+
+ 
+  
 </script>
