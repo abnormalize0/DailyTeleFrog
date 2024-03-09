a18595cad1b9e3dac21751ee633074eede4100b6
Sergei Nabatov <serynabatov@yandex.ru>
2024-03-09 17:28:10 +0100

    first version

---
+++ client/src/components/basic/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-text-field 
+    label="Поиск" 
+    class="search-bar text-secondary-color"
+    variant="outlined"
+    hide-details
+    flat
+    :append-inner-icon="mdi-magnify"
+  >
+  </v-text-field>
+</template>
+
+<style scoped>
+  #input-usage .v-input__prepend-outer,
+  #input-usage .v-input__append-outer,
+  #input-usage .v-input__slot,
+  #input-usage .v-messages {
+    border: 1px dashed rgba(0,0,0, .4);
+  }
+  .search-bar {
+    width: 600px;
+    height: 44px;
+  }
+  .search-bar >>> .v-field {
+    border-radius: 10px !important;
+  }
+</style>
+
+<script>
+  export default {
+    name: "SearchBar",
+    data () {
+      return {
+        text: '',
+      }
+    },
+    methods: {
+      appendIconCallback () {},
+      prependIconCallback () {},
+    },
+  }
+</script>
---
+++ client/src/components/common/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid-container header">
+    <img src="../../assets/Logo.png" alt="Logo" />
+    <SearchBar/>
+    <BasicPrimaryButton content="Войти" class="button-position"/>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+    height: 72px;
+    align-content: center;
+  }
+  .button-position {
+    grid-column: 12
+  }
+</style>
+
+<script>
+import BasicPrimaryButton from '../basic/BasicPrimaryButton.vue';
+import SearchBar from '../basic/SearchBar.vue';
+
+export default {
+  name: "HeaderComponent",
+  components: {
+    BasicPrimaryButton,
+    SearchBar
+  }
+}
+</script>
