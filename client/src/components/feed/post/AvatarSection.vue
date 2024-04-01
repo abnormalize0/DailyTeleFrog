6db59f73c2bd4db1648d162fe21510cef0fb8105
Sergei Nabatov <serynabatov@yandex.ru>
2024-04-01 12:50:10 +0200

    fixed the icon mess

--- client/src/components/feed/post/AvatarSection.vue
+++ client/src/components/feed/post/AvatarSection.vue
@@ -18,146 +18,77 @@
       </div>
       <div class="rightBlockFooter p2">
         <div class="justifyCenter">
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.6667 2H5.33333C4.59695 2 4 2.59695 4 3.33333V14L8 12L12 14V3.33333C12 2.59695 11.403 2 10.6667 2Z"
-              stroke="#292929"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <i class="bookmark-text-color-icon"></i>
           <div>{{ spotlightCount }}</div>
         </div>
         <div class="justifyCenter">
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8 14C11.3137 14 14 11.3137 14 7.99997C14 4.68626 11.3137 1.99997 8 1.99997C4.68629 1.99997 2 4.68626 2 7.99997C2 8.99172 2.24062 9.92728 2.66667 10.7514L2 14L5.24853 13.3333C6.07269 13.7593 7.00825 14 8 14Z"
-              stroke="#292929"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <i class="comment-text-color"></i>
           <div>{{ commentsNumber }}</div>
         </div>
         <div class="justifyCenter">
-          <div class="dislikeCountParent">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="dislikeCountInactive"
-            >
-              <path
-                d="M4.66666 6.66667L7.99999 10"
-                stroke="#DDE5F7"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8 10L11.3333 6.66667"
-                stroke="#DDE5F7"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="dislikeCountActive"
-            >
-              <path
-                d="M4.66666 6.66667L7.99999 10"
-                stroke="#C90C00"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8 10L11.3333 6.66667"
-                stroke="#C90C00"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+          <div>
+            <i class="arrow-down-text-color"></i>
           </div>
           <div>
             {{ likesCount }}
           </div>
-          <div class="likeCountParent">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="likeCountInactive"
-            >
-              <path
-                d="M11.3333 9.33333L8.00001 6"
-                stroke="#DDE5F7"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8 6L4.66667 9.33333"
-                stroke="#DDE5F7"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="likeCountActive"
-            >
-              <path
-                d="M11.3333 9.33333L8.00001 6"
-                stroke="#1CC900"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8 6L4.66667 9.33333"
-                stroke="#1CC900"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+          <div>
+            <i class="arrow-up-text-color"></i>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+  .avatar-section {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .rightBlockHeader {
+    width: 100%;
+  }
+
+  .leftBlock {
+    display: flex;
+    gap: 5px;
+    align-items: center;
+  }
+
+  .justifyRight {
+    display: flex;
+    justify-content: right;
+    gap: 2rem;
+  }
+
+  .rightBlockFooter {
+    display: flex;
+    gap: 3rem;
+    align-items: center;
+  }
+
+  .justifyLeft {
+    display: flex;
+    justify-content: left;
+  }
+
+  .justifyCenter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+  }
+
+  img {
+    height: 60px;
+  }
+</style>
+
 <script>
 export default {
   name: "avatar-section",
@@ -200,74 +131,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-.avatar-section {
-  padding-left: 1rem;
-  padding-right: 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.rightBlockHeader {
-  width: 100%;
-}
-
-.leftBlock {
-  display: flex;
-  gap: 5px;
-  align-items: center;
-}
-
-.justifyRight {
-  display: flex;
-  justify-content: right;
-  gap: 2rem;
-}
-
-.rightBlockFooter {
-  display: flex;
-  gap: 3rem;
-  align-items: center;
-}
-
-.justifyLeft {
-  display: flex;
-  justify-content: left;
-}
-
-.justifyCenter {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 5px;
-}
-
-.dislikeCountActive {
-  display: none;
-}
-
-.dislikeCountParent:hover > .dislikeCountInactive {
-  display: none;
-}
-
-.dislikeCountParent:hover > .dislikeCountActive {
-  display: block;
-}
-
-.likeCountActive {
-  display: none;
-}
-
-.likeCountParent:hover > .likeCountInactive {
-  display: none;
-}
-
-.likeCountParent:hover > .likeCountActive {
-  display: block;
-}
-
-img {
-  height: 60px;
-}
-</style>
