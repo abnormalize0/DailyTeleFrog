16adce26cb2d223388323dbfb15a161043983b55
John Doe <johndoe@example.com>
2024-04-07 21:37:28 +0300

    cleaned everything

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app class="background-main-color">
     <HeaderComponent />
-    <div class="d-flex justify-center">
-      <PostPreview :post="post"></PostPreview>
-    </div>
   </v-app>
 </template>
 
@@ -14,29 +11,11 @@
 <script>
 import HeaderComponent from "@/components/common/HeaderComponent.vue";
 
-import PostPreview from "@/components/feed/post-preview/PostPreview.vue";
+
 export default {
   name: "MVP",
   components: {
     HeaderComponent,
-    PostPreview,
   },
-  data: () => ({
-    post: {
-      watchCount: "42323",
-      clickCount: "1000000",
-      spotlightCount: "49",
-      commentsNumber: "60",
-      likesCount: "-40",
-      accountTag: "@Nintendo",
-      profileName: "Tsugi",
-      avatarImg: "../../../assets/Avatar.png",
-      img: "../../../assets/img-placeholder.png",
-      hashtags: ["bebra", "booba", "chipi"],
-      header: 'Ответ на пост "А вот это интересная точка зрения"',
-      body: "Сама по себе религия - это хорошо. По идее, во всех священных писаниях просто начертались условия выживания. Знаете, зачем нужен вот этот сорокадневный пост? Да чтобы вы последнюю скотину перед летом не сожрали. Сожрете скотину - будет голодный год, Бог вас так карает. А знаете, почему беспорядочный секс - это грех? Да потому что не знал никто раньше про сифилис и гонорею. Вот и думали, вот эти язвы - это их Бог наказывает. Или вот дни без еды, да? По средам и пятницам. Про интервальное голодание они тоже что-то знали.",
-      registrationDate: "25 августа 2023 г"
-    }
-  }),
 };
 </script>
