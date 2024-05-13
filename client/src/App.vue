67ef059030d3d0428ff897e32df6b7a0715b2e66
John Doe <johndoe@example.com>
2024-05-13 22:21:38 +0300

    finished left menu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app class="background-main-color">
     <HeaderComponent />
+    <div style="display:flex">
+      <LeftMenuComponent :groups="groups"/>
+      <PostPreview :post="post"></PostPreview>
+    </div>
+    
   </v-app>
 </template>
 
@@ -10,12 +15,39 @@
 
 <script>
 import HeaderComponent from "@/components/common/HeaderComponent.vue";
-
+import LeftMenuComponent from "@/components/common/LeftMenuComponent.vue";
 
 export default {
   name: "MVP",
   components: {
     HeaderComponent,
+    LeftMenuComponent,
+  },
+  data() {
+    return {
+      groups: [
+        {
+          name: "onanisti",
+          img: "https://cdn2.iconfinder.com/data/icons/people-groups/512/Leader_Avatar-512.png",
+        },
+        {
+          name: "dungeonSlaveZ",
+          img: "https://cdn2.iconfinder.com/data/icons/people-groups/512/Leader_Avatar-512.png",
+        },
+        {
+          name: "boyNextDoor",
+          img: "https://cdn2.iconfinder.com/data/icons/people-groups/512/Leader_Avatar-512.png",
+        },
+        {
+          name: "Jhohan pohan",
+          img: "https://cdn2.iconfinder.com/data/icons/people-groups/512/Leader_Avatar-512.png",
+        },
+        {
+          name: "tester mokaka",
+          img: "https://cdn2.iconfinder.com/data/icons/people-groups/512/Leader_Avatar-512.png",
+        },
+      ],
+    }
   },
 };
 </script>
---
+++ client/src/components/common/LeftMenuComponent.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="parent">
+    <div class="child pointer">
+      <i class="home-icon"></i>
+      <div class="p2 menu-text">Моя лента</div>
+    </div>
+    <div class="child pointer">
+      <i class="popular-icon"></i>
+      <div class="p2 menu-text">Популярное</div>
+    </div>
+    <div class="child pointer">
+      <i class="calendar-icon"></i>
+      <div class="p2 menu-text">Свежее</div>
+    </div>
+    <div>
+      <div class="p1" style="margin-bottom: 24px">Мои сообщества</div>
+      <div v-for="group in filteredGroups" :key="group" class="child pointer">
+        <img class="img" :src="group.img" />
+        <div class="p2 menu-text">{{ group.name }}</div>
+      </div>
+      <div
+        class="p3 pointer"
+        style="
+          margin-bottom: 24px;
+          display: flex;
+          justify-content: space-between;
+        "
+        v-if="showMoreGroups"
+      >
+        БОЛЬШЕ СООБЩЕСТВ
+        <div style="margin-right: 4rem">></div>
+      </div>
+    </div>
+    <div>
+      <div class="p1" style="margin-bottom: 24px">Полезная информация</div>
+      <div class="child pointer">
+        <i class="rules-icon"></i>
+        <div class="p2 menu-text">Правила</div>
+      </div>
+      <div class="child pointer">
+        <i class="advertisement-icon"></i>
+        <div class="p2 menu-text">Заказать рекламу</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.parent {
+  padding: 15px 26px 10px 26px;
+  color: white;
+  width: 20rem;
+}
+.child {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  margin-bottom: 24px;
+}
+.pointer {
+  cursor: pointer;
+}
+.menu-text {
+  padding-left: 8px;
+}
+.img {
+  width: 24px;
+}
+</style>
+
+<script>
+export default {
+  name: "LeftMenuComponent",
+  components: {
+    
+  },
+  props: {
+    groups: []
+  },
+  data() {
+    return {
+      filteredGroups: [],
+      showMoreGroups: false,
+    };
+  },
+  mounted() {
+    this.filterGroups(this.groups);
+  },
+  methods: {
+    filterGroups(groups) {
+      if (groups.length > 4) {
+        this.filteredGroups = groups.slice(0, 4);
+        this.showMoreGroups = true;
+        return;
+      }
+      this.filteredGroups = groups;
+    },
+  },
+};
+</script>
