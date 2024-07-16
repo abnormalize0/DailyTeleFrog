0e032c0fae2f7071ea13573738d1b41afcb6c7bc
John Doe <johndoe@example.com>
2024-07-16 23:15:57 +0300

    infinite scroll early stages

--- client/src/components/common/RightMenuComponent.vue
+++ client/src/components/common/RightMenuComponent.vue
@@ -1,23 +1,27 @@
 <template>
-  <div 
-    class="d-flex 
-      flex-column 
-      primary-rounded 
-      background-secondary-color 
-      align-center wrap-menu"
+  <div
+    class="d-flex flex-column primary-rounded background-secondary-color align-center wrap-menu"
   >
     <div class="h2 text-color">Войти</div>
     <div class="d-flex flex-column w-100 login-form">
-      <BasicInput label="Логин"/>
-      <BasicInput label="Пароль"/>
+      <BasicInput label="Логин" />
+      <BasicInput label="Пароль" />
+      <div class="register-lostpassword-wrap d-flex">
+        <button class="p3 striped-button">Забыли пароль?</button>
+        <button class="p3 striped-button">Регистрация</button>
+      </div>
       <div class="d-flex justify-center publsh-post-button">
-        <BasicPrimaryButton
-          class="w-100"
-          content="Войти"
-        ></BasicPrimaryButton>
+        <BasicPrimaryButton class="w-100" content="Войти"></BasicPrimaryButton>
       </div>
     </div>
   </div>
+  <button
+    @click="topFunction()"
+    class="to-top"
+    style="color: white; display: none; position: sticky; top: 25px"
+  >
+    наверх
+  </button>
 </template>
 
 <style scoped>
@@ -29,6 +33,16 @@
 }
 .login-form {
   gap: 15px;
+}
+.striped-button {
+  background: none !important;
+  border: none;
+  padding: 0 !important;
+  text-decoration: underline;
+  color: white;
+}
+.register-lostpassword-wrap {
+  justify-content: space-between;
 }
 </style>
 
@@ -54,7 +68,28 @@ export default {
       },
     };
   },
-  mounted() {},
-  methods: {},
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      let goToTopButton = document.querySelector(".to-top");
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        goToTopButton.style.display = "block";
+      } else {
+        goToTopButton.style.display = "none";
+      }
+    },
+    topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    },
+  },
 };
 </script>
--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -2,10 +2,13 @@
   <v-container>
     <v-row class="d-flex pt-15">
       <v-col cols="3">
+        <button @click="addMorePosts()" style="color: white">насрать</button>
         <LeftMenuComponent :groups="groups" />
       </v-col>
       <v-col cols="6">
-        <PostPreview :post="post"></PostPreview>
+        <div v-for="post in posts" :key="post">
+          <PostPreview style="margin-bottom: 24px" :post="post"></PostPreview>
+        </div>
       </v-col>
       <v-col cols="3">
         <RightMenuComponent />
@@ -25,6 +28,12 @@ export default {
     PostPreview,
     LeftMenuComponent,
     RightMenuComponent,
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
   data() {
     return {
@@ -50,22 +59,44 @@ export default {
           img: "community-icon",
         },
       ],
-      post: {
-        header: 'Ответ на пост «А вот это интересная точка зрения',
+      ipost: {
+        header: "Ответ на пост «А вот это интересная точка зрения",
         body: "Сама по себе религия - это хорошо. По идее, во всех священных писаниях просто начертались условия выживания. Знаете, зачем нужен вот этот сорокадневный пост? Да чтобы вы последнюю скотину перед летом не сожрали. Сожрете скотину - будет голодный год, Бог вас так карает. А знаете, почему беспорядочный секс - это грех? Да потому что не знал никто раньше про сифилис и гонорею. Вот и думали, вот эти язвы - это их Бог наказывает. Или вот дни без еды, да? По средам и пятницам. Про интервальное голодание они тоже что-то знали.",
-        img: 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
-        hashtags: ['asd', 'asd', 'asd'],
-        avatarImg: 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
-        profileName: 'bebeb',
-        accountTag: '@fff',
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        hashtags: ["asd", "asd", "asd"],
+        avatarImg:
+          "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+        profileName: "bebeb",
+        accountTag: "@fff",
         commentsNumber: 3,
         likesCount: 4,
         clickCount: 2,
         watchCount: 5,
         spotlightCount: 5,
-        registrationDate: 'ffff'
-      }
-    }
+        registrationDate: "ffff",
+      },
+      posts: [],
+    };
   },
-}
+  methods: {
+    addMorePosts() {
+      for (let i = 0; i < 5; i++) {
+        this.posts.push(this.ipost);
+      }
+    },
+    handleScroll() {
+      let bottomOfWindow =
+        Math.max(
+          document.documentElement.scrollTop,
+          document.body.scrollTop
+        ) +
+          window.innerHeight >=
+        document.documentElement.offsetHeight - 100;
+
+      if (bottomOfWindow) {
+        this.addMorePosts();
+      }
+    },
+  },
+};
 </script>
