f2b9ec8b45863f10cfe8cd68bd44a282fd6e1e32
Sergei Nabatov <serynabatov@yandex.ru>
2024-08-16 17:39:35 +0200

    warning problems (#167)

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     watchOptions: {
       ignored: /node_modules/,
       poll: 1000
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
   }
 })
