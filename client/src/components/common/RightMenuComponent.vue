3136f809c38e63c75416909a2705d04546c89134
Sergei Nabatov <serynabatov@yandex.ru>
2024-08-25 20:31:26 +0200

    Right Menu Component fix

--- client/src/components/common/RightMenuComponent.vue
+++ client/src/components/common/RightMenuComponent.vue
@@ -8,25 +8,5 @@ import ProfileComponent from './profile/ProfileComponent.vue';
 export default {
 	name: "RightMenuComponent",
 	components: { ProfileComponent },
-	props: {
-	},
-	data() {
-		return {
-			logedIn: false,
-			username: "",
-			password: "",
-			avatarBlock: {
-				avatarImgSrc: "../../../assets/Avatar.png",
-				subscribers: 30,
-				rating: 2000,
-				patrons: 2,
-				profileName: "koks",
-				profileTag: "@keks",
-			},
-		};
-	},
-	methods: {
-
-  }
 };
 </script>
