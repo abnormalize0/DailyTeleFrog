02e0d42748475a1960d697f0529760d70c99d4af
waverew <waverew@gmail.com>
2024-09-01 14:17:44 +0300

    service integration

--- client/src/api/modules/user.api.js
+++ client/src/api/modules/user.api.js
@@ -6,3 +6,17 @@ export const login = (username, password) => {
         password: password
     });
 }
+
+export const register = (username, password, email) => {
+    return axiosClient.post('/user/register', {
+        username: username,
+        password: password,
+        email: email
+    });
+}
+
+export const forgotPassword = (email) => {
+    return axiosClient.post('/forgot/password', {
+        email: email,
+    });
+}
--- client/src/services/modules/user.service.js
+++ client/src/services/modules/user.service.js
@@ -4,3 +4,13 @@ export const login = async (username, password) => {
     const { data } = await UserApi.login(username, password);
     return data;
 }
+
+export const register = async (username, password, email) => {
+    const { data } = await UserApi.register(username, password, email);
+    return data;
+}
+
+export const forgotPassword = async (email) => {
+    const { data } = await UserApi.forgotPassword(email);
+    return data;
+}
