8340a2f6c1e050024d9e3f22e93981a0f340d2c3
Sergei Nabatov <serynabatov@yandex.ru>
2025-02-09 23:03:20 +0100

    letters

---
+++ client/src/components/add-post/text-frame/Letter.vue
@@ -0,0 +1,65 @@
+<template>
+  <button 
+    class="d-flex letter p2 not-active-color secondary-rounded justify-center align-center" 
+    :class="applyStyle"
+  >
+    {{ this.letter }}
+  </button>
+</template>
+
+<style scoped>
+.letter {
+  width: 16px;
+  height: 16px;
+}
+
+.letter:hover {
+  border: 1px solid var(--not-active-color);
+}
+
+.letter:focus {
+  color: var(--active-color) !important;
+}
+</style>
+
+<script>
+import { ParagraphState } from '@/enum/ParagraphState';
+
+export default {
+  name: "Letter",
+  props: {
+    type: {
+      type: ParagraphState,
+      default: undefined,
+    },
+  },
+  data: () => ({
+    letter: ""
+  }),
+  computed: {
+    applyStyle() {
+      if (!this.type) {
+        return;
+      }
+      if (this.type === ParagraphState.BOLD) {
+        this.letter = "B";
+        return "bold";
+      }
+      if (this.type === ParagraphState.ITALIC) {
+        this.letter = "I";
+        return "italic";
+      }
+      if (this.type === ParagraphState.UNDERLINE) {
+        this.letter = "U";
+        return "underline";
+      }
+      if (this.type === ParagraphState.STRIKETHROUGH) {
+        this.letter = "S";
+        return "strikethrough";
+      }
+      this.letter = "C";
+      return;
+    }
+  }
+}
+</script>
